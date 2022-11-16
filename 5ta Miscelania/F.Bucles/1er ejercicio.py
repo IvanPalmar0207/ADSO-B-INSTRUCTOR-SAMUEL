8e0a93b75a914f8591a9604856248721e478f354
ivanpalmar07 <palmar.ivan0205@gmail.com>
2022-11-16 01:28:22 -0500

    5ta miscelania(Funciones)

---
+++ 5ta Miscelania/F.Bucles/1er ejercicio.py
@@ -0,0 +1,10 @@
+'''Determinar si el numero es o no es par'''
+
+def parimpar(num=int(input("Ingrese un numero: "))):
+    while num>0:
+        if num%2==0:
+            return "El numero ingresado es par"
+        else:
+            return "El numero ingresado no es par"
+
+print(parimpar())
---
+++ 5ta Miscelania/F.Bucles/4to ejercicio.py
@@ -0,0 +1,9 @@
+'''Determinar cuales son los múltiplos de 5 comprendidos entre
+1 y N.'''
+def multiplo(num=int(input("ingrese un numero: "))):
+    for i in range(1,num+1):
+        if i%5==0:
+            print(i,"es divisor de 5")
+        else:
+            print(i,'No es divisor de 5')
+multiplo()
