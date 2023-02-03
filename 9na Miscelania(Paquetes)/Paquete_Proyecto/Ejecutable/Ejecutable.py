994f9bbd9c92de97a569d529c134ce967a163687
Ivan Palmar <palmar.ivan0205@gmail.com>
2023-02-03 01:51:16 -0500

    9na Miscelania(Paquetes)

---
+++ 9na Miscelania(Paquetes)/Paquete_Proyecto/Ejecutable/Ejecutable.py
@@ -0,0 +1,6 @@
+from sys import path
+
+path.append('C:\\Users\\palma\\OneDrive\\Documents\\Ivan Palmar\\SENA\\3er Trimestre\\Samuel Padilla\\Repositorio\\ADSO-B-INSTRUCTOR-SAMUEL\\9na Miscelania(Paquetes)\\Paquete_Proyecto')
+import matematicas.area_pr.area
+
+#print(matematicas.area_pr.area.a_elipse()) -Codigo de ejemplo:)
