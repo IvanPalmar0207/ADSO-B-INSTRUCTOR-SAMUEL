6fcb44217b712f2f43250c6afc5753a9c1533313
Ivan Palmar <palmar.ivan0205@gmail.com>
2023-03-12 14:29:38 -0500

    Comentarios(Archivos)

--- 14ava Miscelania (archivos)/Arreglo (Carga)/Conductor.py
+++
@@ -1,8 +0,0 @@
-class Conductor: #Se hace uso de la palabra reservada class que sirve
-    def __init__(self,nombre,documento):
-        self.__nombre=nombre
-        self.__documento=documento
-    def getNombre(self):
-        return self.__nombre
-    def getDocumento(self):
-        return self.__documento
